6e4c6c5e4eb4f7bdd759046b13874cc2182fb8c8
iyawn <iannmacabulos@gmail.com>
2025-07-12 17:49:43 +0800

    Added Checkout Animation

--- checkout.php
+++ checkout.php
@@ -111,20 +111,30 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET') {
                     <div class="checkout-footer">
                         <div class="total-and-confirm">
                             <p><strong>Total: ₱<?= number_format($total, 2) ?></strong></p>
-                            <div data-tooltip="Total: ₱<?= number_format($total, 2) ?>" class="kusinago-confirm-btn">
-                                <div class="kusinago-btn-wrapper">
-                                    <button type="submit" data-tooltip="Total: ₱<?= number_format($total, 2) ?>" class="kusinago-confirm-btn"></button> <!-- not </div> -->
-                                    <span class="kusinago-btn-text">Confirm & Pay</span>
-                                    <span class="kusinago-btn-icon">
-                                        <svg viewBox="0 0 16 16" fill="currentColor" height="16" width="16" xmlns="http://www.w3.org/2000/svg">
-                                            <path d="M0 2.5A.5.5 0 0 1 .5 2H2a.5.5 0 0 1 .485.379L2.89 4H14.5a.5.5 0 0 1 .485.621l-1.5 6A.5.5 0 0 1 13 11H4a.5.5 0 0 1-.485-.379L1.61 3H.5a.5.5 0 0 1-.5-.5zM3.14 5l1.25 5h8.22l1.25-5H3.14zM5 13a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0zm9-1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0z"></path>
+
+                            <button
+                                type="submit"
+                                class="checkout-confirm-btn"
+                                data-tooltip="Total: ₱<?= number_format($total, 2) ?>">
+
+                                <div class="checkout-btn-wrapper">
+                                    <span class="checkout-btn-text">Confirm & Pay</span>
+                                    <span class="checkout-btn-icon">
+                                        <svg viewBox="0 0 16 16" fill="currentColor" height="20" width="20" xmlns="http://www.w3.org/2000/svg">
+                                            <path d="M0 2.5A.5.5 0 0 1 .5 2H2a.5.5 0 0 1 .485.379L2.89 4H14.5a.5.5 
+              0 0 1 .485.621l-1.5 6A.5.5 0 0 1 13 11H4a.5.5 0 0 
+              1-.485-.379L1.61 3H.5a.5.5 0 0 1-.5-.5zM3.14 
+              5l1.25 5h8.22l1.25-5H3.14zM5 13a1 1 0 1 0 0 2 
+              1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 
+              0zm9-1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 
+              0 1 1 4 0 2 2 0 0 1-4 0z" />
                                         </svg>
                                     </span>
-                                    
                                 </div>
-                            </div>
+                            </button>
                         </div>
                     </div>
+
                 </form>
             </div>
         </div>
