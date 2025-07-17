5401387c9faf4c3aaaca0270f96d9221dbf082f4
iyawn <iannmacabulos@gmail.com>
2025-07-17 14:18:42 +0800

    Fixed Navbar Links

--- include/header.php
+++ include/header.php
@@ -6,7 +6,6 @@ if (session_status() === PHP_SESSION_NONE) {
 $loggedInUser = $_SESSION['user'] ?? null;
 $loggedInAdmin = $_SESSION['admin'] ?? null;
 
-//
 $cartCount = 0;
 if (isset($_SESSION['cart'])) {
   foreach ($_SESSION['cart'] as $item) {
@@ -14,7 +13,6 @@ if (isset($_SESSION['cart'])) {
   }
 }
 
-// 
 $pendingCount = 0;
 if ($loggedInAdmin) {
   $client = new MongoDB\Client("mongodb://localhost:27017");
@@ -23,22 +21,21 @@ if ($loggedInAdmin) {
 }
 ?>
 
+<script src="https://code.iconify.design/iconify-icon/1.0.7/iconify-icon.min.js"></script>
 <link rel="stylesheet" href="css/main.css">
 <link rel="stylesheet" href="css/responsive.css">
+
 <header class="header">
   <div class="header-container">
-    <!-- Left Nav -->
+
+    <!-- Left Navigation -->
     <div class="nav-left">
       <?php if ($loggedInUser): ?>
+        <a href="index.php">Home</a>
         <a href="menu.php">Menu</a>
-        <a class="login-btn btn-badge" href="cart.php">
-          Cart
-          <?php if ($cartCount > 0): ?>
-            <span class="badge cart-badge"><?= $cartCount ?></span>
-          <?php endif; ?>
-        </a>
       <?php elseif ($loggedInAdmin): ?>
         <a href="dashboard.php">Dashboard</a>
+        <a href="menu_list.php">Menu</a>
         <a href="admin_orders.php" class="btn-badge">
           Orders
           <?php if ($pendingCount > 0): ?>
@@ -48,27 +45,41 @@ if ($loggedInAdmin) {
       <?php endif; ?>
     </div>
 
-    <?php
-    $isAdmin = isset($_SESSION['admin']);
-    $logoLink = $isAdmin ? 'dashboard.php' : 'index.php';
-    ?>
+    <!-- Center Logo -->
     <div class="nav-center">
+      <?php $logoLink = $loggedInAdmin ? 'dashboard.php' : 'index.php'; ?>
       <a href="<?= $logoLink ?>">
         <img src="uploads/KusinoGo Logo.svg" alt="KusinaGo Logo" class="logo">
       </a>
     </div>
 
-    <!-- Right Nav -->
+    <!-- Right Navigation -->
     <div class="nav-right">
       <?php if ($loggedInUser): ?>
+        <a href="cart.php" class="btn-badge">
+          Cart
+          <?php if ($cartCount > 0): ?>
+            <span class="badge cart-badge"><?= $cartCount ?></span>
+          <?php endif; ?>
+        </a>
         <a href="user_orders.php">My Orders</a>
-        <a href="logout.php">Logout</a>
       <?php elseif ($loggedInAdmin): ?>
+        <a href="admin_inventory.php">Inventory</a>
         <a href="admin_report.php">Sales</a>
-        <a href="logout.php">Logout</a>
+        <a href="admin_users.php">User Stats</a>
+      <?php endif; ?>
+    </div>
+
+    <!-- Logout Icon -->
+    <div class="nav-logout">
+      <?php if ($loggedInUser || $loggedInAdmin): ?>
+        <a href="logout.php" class="logout-icon" title="Logout">
+          <iconify-icon icon="mdi:logout"></iconify-icon>
+        </a>
       <?php else: ?>
         <a href="login.php">Login</a>
       <?php endif; ?>
     </div>
+
   </div>
 </header>
